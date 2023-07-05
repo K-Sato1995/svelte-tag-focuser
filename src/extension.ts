da30ec8c870e8b60b74b064bb9268ed2d704674b
Segovo <44536814+segovo@users.noreply.github.com>
2023-07-05 16:47:56 -0500

    Focus on HTML

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-async function focusTag(editor: vscode.TextEditor, tagName: string): Promise<void> {
+async function focusTag(editor: vscode.TextEditor, tagName: string): Promise<void> { 
     const content = editor.document.getText();
     const tagRegex = new RegExp(`<${tagName}[^>]*>([\\s\\S]*?)<\\/${tagName}>`, 'gi');
     const match = tagRegex.exec(content);
@@ -8,6 +8,18 @@ async function focusTag(editor: vscode.TextEditor, tagName: string): Promise<voi
         const startPosition = editor.document.positionAt(match.index);
         const endPosition = editor.document.positionAt(match.index + match[0].length);
         const range = new vscode.Range(startPosition, endPosition);
+        editor.revealRange(range, vscode.TextEditorRevealType.AtTop);
+        editor.selection = new vscode.Selection(startPosition, startPosition);
+    }
+}
+
+async function focusHTML(editor: vscode.TextEditor): Promise<void> { 
+    const content = editor.document.getText();
+    const tagRegex = new RegExp(`<script[^>]*>([\\s\\S]*?)<\\/script>`, 'gi');
+    const match = tagRegex.exec(content);
+    if (match) {
+        const position = editor.document.positionAt(match.index + match[0].length);
+        const range = new vscode.Range(position, position);
         editor.revealRange(range, vscode.TextEditorRevealType.AtTop);
         editor.selection = new vscode.Selection(startPosition, startPosition);
     }
@@ -23,6 +35,14 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
+    // Create a command to focus on the HTML
+    let htmlCommand = vscode.commands.registerCommand('svelte-tag-focuser.focusOnHTML', () => {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor) {
+            foucsHTML(activeEditor);
+        }
+    });
+
     // Create a command to focus on the style tag
     let styleCommand = vscode.commands.registerCommand('svelte-tag-focuser.focusOnStyle', () => {
         const activeEditor = vscode.window.activeTextEditor;
@@ -31,5 +51,5 @@ export function activate(context: vscode.ExtensionContext) {
         }
     });
 
-    context.subscriptions.push(scriptCommand, styleCommand);
+    context.subscriptions.push(scriptCommand, htmlCommand, styleCommand);
 }
